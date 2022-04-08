c0f80cdfb21cdfff5ab568f528bb0073416ec92c
JehanKandy <89431184+JehanKandy@users.noreply.github.com>
2022-04-08 15:15:13 +0530

    Add files via upload

---
+++ time.php
@@ -0,0 +1,37 @@
+<?php
+    date_default_timezone_set('Asia/Kolkata');
+    $today = date('Y-m-d');
+    $after_date = date('Y-m-d',strtotime('+30 days',strtotime(str_replace('/', '-',$today))));
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Check Time</title>
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
+</head>
+<body>
+<div class="container">
+    <br><br>
+    <h4>PHP Date Add For Current Date</h4>
+
+    Today's Date : <?php echo $today; ?>
+    <br><br>
+    and the 
+    <br>
+    After 30 days,
+    <br><br>
+    date is : <?php echo $after_date; ?>
+</div> 
+
+
+
+<script src="https://code.jquery.com/jquery-3.2.1.slim.min.js" integrity="sha384-KJ3o2DKtIkvYIK3UENzmM7KCkRr/rE9/Qpg6aAZGJwFDMVNA/GpGFF93hXpG5KkN" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/popper.js@1.12.9/dist/umd/popper.min.js" integrity="sha384-ApNbgh9B+Y1QKtv3Rn7W3mgPxhU9K/ScQsAP7hUibX39j7fakFPskvXusvfa0b4Q" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.0.0/dist/js/bootstrap.min.js" integrity="sha384-JZR6Spejh4U02d8jOt6vLEHfe/JQGiRRSQQxSfFWpi1MquVdAyjUar5+76PVCmYl" crossorigin="anonymous"></script>
+</body>
+</html>
